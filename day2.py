c9cf6e1f8e06c7a1e76e8766c739e7a1fbfe2492
Clayton Kristiansen <clayton.kristiansen@hotmail.com>
2024-12-11 13:47:30 -0600

    Finished day 2

--- day2.py
+++ day2.py
@@ -1,35 +1,33 @@
+from pathlib import Path
+
 data = []
 
-with open('/home/a0500846/AdventOfCode2024/day2/input.txt', 'r') as f:
+with open(Path(__file__).parent / 'input.txt', 'r') as f:
     for line in f.readlines():
         row = [int(x) for x in line.strip().split()]
         data.append(row)
 
 num_safe_reports = 0
 
+
+# Currently setup for day 2, allows removing one element to still be safe
 for levels in data:
-    safe = True
-    increasing = True
-    have_removed = False
-    if levels[0] - levels[1] > 0:
-        increasing = False
-    prev_level = levels[0]
-    for i in range(1, len(levels)):
-        if levels[i - 1] == levels[i] \
-           or (increasing and (levels[i - 1] - levels[i] > 0)) \
-           or ((not increasing) and (levels[i - 1] - levels[i] < 0)) \
-           or (abs(levels[i - 1] - levels[i]) > 3):
-            if not have_removed:
-                if i == (len(levels) - 1):
-                    
-                if levels[i - 1] == levels[i] \
-                   or (increasing and (levels[i - 1] - levels[i] > 0)) \
-                   or ((not increasing) and (levels[i - 1] - levels[i] < 0)) \
-                   or (abs(levels[i - 1] - levels[i]) > 3):
-            else:
+    for skip_index in range(0, len(levels)):
+        mod_level = levels[:skip_index] + levels[skip_index + 1:]
+        safe = True
+        increasing = True
+        if mod_level[0] - mod_level[1] > 0:
+            increasing = False
+        prev_level = mod_level[0]
+        for i in range(1, len(mod_level)):
+            if mod_level[i - 1] == mod_level[i] \
+            or (increasing and (mod_level[i - 1] - mod_level[i] > 0)) \
+            or ((not increasing) and (mod_level[i - 1] - mod_level[i] < 0)) \
+            or (abs(mod_level[i - 1] - mod_level[i]) > 3):
                 safe = False
                 break
-    if safe:
-        num_safe_reports += 1
+        if safe:
+            num_safe_reports += 1
+            break
 
 print(f"The number of safe reports is {num_safe_reports}")
