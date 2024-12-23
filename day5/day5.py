94fbc80cdb39024db7899af8906a18df1bc17eda
Clayton Kristiansen <clayton.kristiansen@hotmail.com>
2024-12-22 22:35:10 -0800

    Complete day 5

--- day5/day5.py
+++ day5/day5.py
@@ -1,6 +1,7 @@
-# Advent of Code 2024 day 4
+# Advent of Code 2024 day 5
 
 from pathlib import Path
+from functools import cmp_to_key
 import re
 
 def CheckRule(update, rule):
@@ -12,6 +13,15 @@ def CheckRule(update, rule):
             return False
     return True
 
+def InOrder(item1, item2, rules):
+    for rule in rules:
+        if rule[0] == item1 and rule[1] == item2:
+            return -1
+        if rule[1] == item1 and rule[0] == item2:
+            return 1
+    return 0
+
+
 data = ""
 
 with open(Path(__file__).parent / 'input.txt', 'r') as f:
@@ -22,6 +32,7 @@ rules = [[int(num) for num in line.split('|')] for line in data[0].split('\n')]
 updates = [[int(num) for num in line.split(',')] for line in data[1].split('\n')]
 
 good_updates = []
+bad_updates = []
 sum_of_middle = 0
 
 for update in updates:
@@ -32,6 +43,30 @@ for update in updates:
     if is_good:
         good_updates.append(update)
         sum_of_middle += update[int(len(update) / 2)]
+    else:
+        bad_updates.append(update)
 
 # print(good_updates)
 print(sum_of_middle)
+
+print(bad_updates[0])
+sorted_updates = []
+sorted_middle_sum = 0
+
+for update in bad_updates:
+    sorted_update = sorted(update, key=cmp_to_key(lambda item1, item2: InOrder(item1, item2, rules)))
+    sorted_middle_sum += sorted_update[int(len(update) / 2)]
+    sorted_updates.append(sorted_update)
+
+
+print(sorted_updates[0])
+
+for update in sorted_updates:
+    is_good = True
+    for rule in rules:
+        if not CheckRule(update, rule):
+            is_good = False
+    if not is_good:
+        print("FAILED")
+
+print(sorted_middle_sum)
